d4c65bd1315dc1e2843f99723c8d20dc9cd670e9
oviedo84 <oviedoinfinite@hotmail.com>
2023-12-25 22:56:47 -0600

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const vetRouter = require("./routers/vet");
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors);
 
 app.use(vetRouter);
 
